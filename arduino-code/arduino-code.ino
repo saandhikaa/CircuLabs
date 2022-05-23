5260d4e4cb0056677bdcf0d1ab40126902a8dabf
gilang sandhika putra <gilangsandhika_putra@yahoo.com>
2022-05-23 23:52:12 +0700

    create LDR sensor test

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,84 +1,73 @@
-#include <SoftwareSerial.h>
-  SoftwareSerial esp(2,3);  // RX, TX
+#define LED_ON  digitalWrite(5,HIGH)
+#define LED_OFF digitalWrite(5,LOW)
 
-String ssid = "sweet";
-String password = "Tiramisu";
+const uint8_t MyAnti[3] = {A2, A1, A0};  // Anti A, Anti B, Anti D
+const int Anti[3] = {950, 950, 950};  // Anti A, Anti B, Anti D
+const int N = 100;
 
-String server = "eoiigimyy5mb7kp.m.pipedream.net";
-String path = "/verifying.php";
+//////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  esp.begin(9600);
   Serial.begin(9600);
   Serial.println("\nSTART\n");
   
-  esp_reset();
-  String data[] = {"A","POSITIF"};
-  httppost(data);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(5,OUTPUT);
+
+  fungsi_utama();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
 
-void esp_reset() {
-  esp.println("AT+RST");
-  delay(1000);
-  if (esp.find("OK")){
-    Serial.println("Module: Reset");
-    connectWifi();
-  }
-}
+//////////////////////////////////////////////////////////////////////////
 
-void connectWifi() {
-  String cmd = "AT+CWJAP=\"" + ssid + "\",\"" + password + "\"";
-  esp.println(cmd);
+void fungsi_utama() {
+  int value[100];
+  boolean sensor[3];
+  String result[2];
+
+  LED_ON;
   delay(4000);
-  if (esp.find("OK")) {
-    Serial.println("WiFi: Connected!\n");
-  } else {
-    Serial.print(".");
-    connectWifi();
+
+  for (int s = 0; s < 3; s++){
+    for (int a = 0; a < N; a++){
+      value[a] = analogRead(MyAnti[s]);
+      delay(20);
+    }
+    sensor[s] = kalkulasi(value,s) >= 50 ? true : false ;
   }
+  
+  LED_OFF;
+  
+  if (   sensor[0] && ! sensor[1]) result[0] = "A";  else
+  if ( ! sensor[0] &&   sensor[1]) result[0] = "B";  else
+  if (   sensor[0] &&   sensor[1]) result[0] = "AB"; else
+  if ( ! sensor[0] && ! sensor[1]) result[0] = "O";
+  
+  result[1] = sensor[2] ? "POSITIF" : "NEGATIF";
+
+  Serial.println("\nDarah  : " + result[0] + String(result[1] == "POSITIF" ? "+" : "-") + "\n\n");
+
 }
 
+int kalkulasi(int value[100], int n) {
+  char x[3] = "ABD";
+  int result = 0;
+  long mean  = 0;
 
-void httppost (String value[2]){
-  String content = "types=" + value[0] + "&rhesus=" + value[1];
-  
-  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");
-  
-  if (esp.find("OK")) Serial.println("TCP connection ready\n");
-  delay(1000);
-
-  String postRequest =
-    "POST " + path + " HTTP/1.0\r\n" +
-    "Host: " + server + "\r\n" +
-    "Accept: *" + "/" + "*\r\n" +
-    "Content-Length: " + content.length() + "\r\n" +
-    "Content-Type: application/x-www-form-urlencoded\r\n" +
-    "\r\n" + content;
-
-  Serial.println("Post Request:\n\n" + postRequest + "\n");
-    
-  esp.println("AT+CIPSEND=" + String(postRequest.length()));
-  delay(500);
-  
-  if (esp.find(">")){
-    Serial.print("Sending...");
-    esp.print(postRequest);
-    
-    if (esp.find("SEND OK")){
-      Serial.println(" > Packet sent!\n\n");
-      
-      while (esp.available()){
-        String tmpResp = esp.readString();
-        Serial.println(tmpResp);
-      }
-      
-      esp.println("AT+CIPCLOSE");
-    }
-    
+  for (int s = 0; s < N; s++){
+    if (value[s] >= Anti[n]) result++;
+    mean+=value[s];
   }
+
+  result = result/N*100;
+  mean /= N;
+  Serial.print("Anti " + String(x[n]) + " : " + String(result) + "%");
+  Serial.println("\tLDR : " + String(mean));
+  return result;
 }
